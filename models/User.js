a4054014fd57095c01fd08f30629d7b09e698085
James Prince <james.prince1@gmail.com>
2022-12-02 13:03:11 +1100

    added an beforeUpdate hook for password hashing

--- models/User.js
+++ models/User.js
@@ -42,6 +42,12 @@ User.init(
                 newUser.password = await bcrypt.hash(newUser.password, 10);
                 return newUser;
             },
+            beforeUpdate: async (updatedUser) => {
+                if (updatedUser.password){
+                    updatedUser.password = await bcrypt.hash(updatedUser.password, 10);
+                }
+                return updatedUser;
+            }
         },
         sequelize,
         timestamps: false,
